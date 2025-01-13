88e6b01b54a804573c00c8b648897cff0e639a3d
Miska Vivolin <miska.vivolin@gmail.com>
2025-01-13 11:49:52 +0200

    added multithreading

--- src/main/java/com/httpserver/Httpserver.java
+++ src/main/java/com/httpserver/Httpserver.java
@@ -2,56 +2,34 @@ package com.httpserver;
 
 import com.httpserver.config.Configuration;
 import com.httpserver.config.ConfigurationManager;
-
+import com.httpserver.core.ServerListenerThread;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
 
 /*
 driver class for http server
  */
 public class Httpserver {
 
+    private final static Logger LOGGER = LoggerFactory.getLogger(Httpserver.class);
+
     public static void main(String[] args) {
 
-        System.out.println("server starting...");
-
+        LOGGER.info("server starting...");
         ConfigurationManager.getInstance().loadConfigurationFile("src/main/resources/http.json");
         Configuration conf = ConfigurationManager.getInstance().getCurrentConfiguration();
 
-        System.out.println("Using Port: " + conf.getPort());
-        System.out.println("Using Webroot: " + conf.getWebroot());
+        LOGGER.info("Using Port: " + conf.getPort());
+        LOGGER.info("Using Webroot: " + conf.getWebroot());
 
         try {
-            ServerSocket serverSocket = new ServerSocket(conf.getPort());
-            Socket socket = serverSocket.accept();
-
-            InputStream inputStream = socket.getInputStream();
-            OutputStream outputStream = socket.getOutputStream();
-
-            String html = "<html><head><title>simple Java http server</title></head><body><h1>This page was served using a simple Java http server</h1></body></html>";
-
-            final String CRLF = "\n\r"; // 13, 10
-
-            String response =
-                    "HTTP/1.1 200 OK" + CRLF +// Status Line : HTTP VERSION RESPONSE_CODE RESPONSE_MESSAGE;
-                    "Content-Length: " + html.getBytes().length + CRLF + // Header
-                            CRLF +
-                            html +
-                            CRLF + CRLF;
-
-            outputStream.write(response.getBytes());
-
-            inputStream.close();
-            outputStream.close();
-            socket.close();
-            serverSocket.close();
-
+            ServerListenerThread serverListenerThread = new ServerListenerThread(conf.getPort(), conf.getWebroot());
+            serverListenerThread.start();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
     }
 
 }
---
+++ src/main/java/com/httpserver/core/HttpConnectionWorkerThread.java
@@ -0,0 +1,65 @@
+package com.httpserver.core;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class HttpConnectionWorkerThread extends Thread {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(HttpConnectionWorkerThread.class);
+    private Socket socket;
+    public HttpConnectionWorkerThread(Socket socket) {
+        this.socket = socket;
+    }
+    @Override
+    public void run() {
+        InputStream inputStream = null;
+        OutputStream outputStream = null;
+        try {
+            inputStream = socket.getInputStream();
+            outputStream = socket.getOutputStream();
+
+            String html = "<html><head><title>simple Java http server</title></head><body><h1>This page was served using a simple Java http server</h1></body></html>";
+
+            final String CRLF = "\n\r"; // 13, 10
+
+            String response =
+                    "HTTP/1.1 200 OK" + CRLF +// Status Line : HTTP VERSION RESPONSE_CODE RESPONSE_MESSAGE;
+                            "Content-Length: " + html.getBytes().length + CRLF + // Header
+                            CRLF +
+                            html +
+                            CRLF + CRLF;
+
+            outputStream.write(response.getBytes());
+
+            inputStream.close();
+            outputStream.close();
+            socket.close();
+
+            LOGGER.info("Connection processing finished");
+        } catch (IOException e) {
+            LOGGER.error("problem with communication", e);
+            e.printStackTrace();
+        } finally {
+            if (inputStream!=null) {
+                try {
+                    inputStream.close();
+                }catch (IOException e) {}
+            }
+            if (outputStream!=null) {
+                try {
+                    outputStream.close();
+                }catch (IOException e) {}
+            }
+            if (socket!=null) {
+                try {
+                    socket.close();
+                } catch (IOException e) {}
+            }
+        }
+    }
+}
